a84fc4b97ef324d4a99edcc3e06f3448afe4d16a
hruden <119438895+hruden@users.noreply.github.com>
2023-08-06 16:20:47 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -56,7 +56,7 @@ export class App extends Component {
 
         <h2>Contacts</h2>
         <Filter searchContact={this.state.searchContact} handleFind={this.handleFind}/>
-        {this.findContact().length ? (<ContactList contacts={this.findContact()} removeContact={this.removeContact}/>
+        {this.state.contacts.length ? (<ContactList contacts={this.findContact()} removeContact={this.removeContact}/>
         ):(<p>No matches found!</p>)} 
       </Container>
     );
